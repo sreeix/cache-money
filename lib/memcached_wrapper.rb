d4f3f1e310310be0c14f59242f93a1c5c62ab390
V Sreekanth <sreeix@gmail.com>
2012-03-03 18:04:41 +0530

    Wrapping dalli client with memcache wapprer

--- lib/memcached_wrapper.rb
+++ lib/memcached_wrapper.rb
@@ -1,6 +1,14 @@
 require 'dalli'
-# require 'dalli/memcache-client'
+require 'dalli/memcache-client'
 
 
 class MemcachedWrapper < Dalli::Client
+  def get(key, raw =false)
+    super(key, :raw => raw)
+  end
+
+  def set(key, value, expire = @default_ttl, raw = false)
+    $memcache.set(key, value, expire, :raw => raw) if active?
+  end
+
 end
